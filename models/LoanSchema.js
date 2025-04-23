c60eacef4650ed67832897d5108c34acedd4ef6e
sayuj <sayuj.sehgal@gmail.com>
2025-04-23 22:02:45 +0530

    add support of start & end date in loan

--- models/LoanSchema.js
+++ models/LoanSchema.js
@@ -2,18 +2,26 @@ import mongoose from "mongoose";
 
 const LoanSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  loanType: { type: String, enum: ["taken", "given"], required: true }, 
-  borrowerName: { type: String, required: function () { return this.loanType === "taken"; } }, 
-  lenderName: { type: String, required: function () { return this.loanType === "given"; } }, 
-  loanCategory: { 
-    type: String, 
-    required: true 
-  }, 
+  loanType: { type: String, enum: ["taken", "given"], required: true },
+  borrowerName: {
+    type: String,
+    required: function () {
+      return this.loanType === "taken";
+    },
+  },
+  lenderName: {
+    type: String,
+    required: function () {
+      return this.loanType === "given";
+    },
+  },
+  loanCategory: { type: String, required: true },
   amount: { type: Number, required: true },
   interestRate: { type: Number, default: 0 },
-  tenureMonths: { type: Number, required: false }, //optional
+  tenureMonths: { type: Number, required: false }, // optional
   emiAmount: { type: Number, required: false },
-  dueDate: { type: Date, required: false },
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: false },
   status: { type: String, default: "active" },
   payments: [
     {
@@ -21,30 +29,58 @@ const LoanSchema = new mongoose.Schema({
       date: { type: Date, default: Date.now },
     },
   ],
-  remainingBalance: { type: Number, default: function () { return this.amount; } },
+  remainingBalance: {
+    type: Number,
+    default: function () {
+      return this.amount;
+    },
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
-LoanSchema.pre("save", function (next) {
-  if (this.tenureMonths && this.interestRate > 0) {
-    const monthlyInterest = this.interestRate / 100 / 12;
-    const emi =
-      (this.amount * monthlyInterest * Math.pow(1 + monthlyInterest, this.tenureMonths)) /
-      (Math.pow(1 + monthlyInterest, this.tenureMonths) - 1);
-    this.emiAmount = Math.round(emi);
+LoanSchema.methods.updateEMI = function () {
+  let tenure = this.tenureMonths;
+
+  if (!tenure && this.startDate && this.endDate) {
+    const start = new Date(this.startDate);
+    const end = new Date(this.endDate);
+    tenure =
+      (end.getFullYear() - start.getFullYear()) * 12 +
+      (end.getMonth() - start.getMonth());
   }
 
-  this.remainingBalance = this.amount - this.payments.reduce((sum, p) => sum + p.amount, 0);
+  if (tenure) {
+    if (this.interestRate > 0) {
+      const monthlyInterest = this.interestRate / 100 / 12;
+      const remainingAmount = this.remainingBalance;
+      const emi =
+        (remainingAmount *
+          monthlyInterest *
+          Math.pow(1 + monthlyInterest, tenure)) /
+        (Math.pow(1 + monthlyInterest, tenure) - 1);
+      this.emiAmount = Math.round(emi);
+    } else {
+      // No-cost EMI: simply divide remaining balance over months
+      this.emiAmount = Math.round(this.remainingBalance / tenure);
+    }
+  }
+};
+
+LoanSchema.pre("save", function (next) {
+  this.remainingBalance =
+    this.amount - this.payments.reduce((sum, p) => sum + p.amount, 0);
 
   if (this.remainingBalance <= 0) {
     this.remainingBalance = 0;
     this.status = "completed";
-  } else if (this.dueDate && new Date() > this.dueDate) {
+  } else if (this.endDate && new Date() > this.endDate) {
     this.status = "overdue";
   } else {
     this.status = "active";
   }
 
+  // Always recalculate EMI
+  this.updateEMI();
   next();
 });
 
