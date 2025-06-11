834491fca9490526c44ce716275512fda7c3e1ee
sayuj <sayuj.sehgal@gmail.com>
2025-06-11 09:38:37 +0530

    refactor loading overlay: change height to 50vh and remove fixed positioning

--- components/shoppings/ShoppingListManager.js
+++ components/shoppings/ShoppingListManager.js
@@ -227,11 +227,8 @@ const ShoppingListManager = () => {
       {loading ? (
         <div
           style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
             width: "100%",
-            height: "100%",
+            height: "50vh",
             backgroundColor: "rgba(255, 255, 255, 0.7)",
             display: "flex",
             justifyContent: "center",
