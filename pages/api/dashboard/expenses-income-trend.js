3ad1711243fcbe17d268db1f4e41203c750b144f
sayuj <sayuj.sehgal@gmail.com>
2025-07-16 10:58:26 +0530

    refactor: clean up unused code and comments in expenses-income-trend API

--- pages/api/dashboard/expenses-income-trend.js
+++ pages/api/dashboard/expenses-income-trend.js
@@ -4,9 +4,7 @@ import connectDB from "@/lib/mongodb";
 import Transaction from "@/models/TransactionSchema";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
-
 import timezone from "dayjs/plugin/timezone";
-
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -18,7 +16,6 @@ export default async function handler(req, res) {
   if (!userId) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
 
   switch (req.method) {
     case "GET":
@@ -45,10 +42,7 @@ export default async function handler(req, res) {
         const monthlyMap = {};
 
         transactions.forEach((tx) => {
-
           const monthKey = dayjs.utc(tx.date).tz("Asia/Kolkata").format("MMM YYYY");
-          //dayjs(tx.date).format("MMM YYYY"); //date.toLocaleString("default", { month: "short" }) + " " + date.
-          console.log("monthKey", tx.date, tx.amount, tx.type);
           if (!monthlyMap[monthKey]) {
             monthlyMap[monthKey] = { income: 0, expense: 0 };
           }
@@ -57,26 +51,6 @@ export default async function handler(req, res) {
         });
 
         // // Step 3: Convert map to sorted array
-        // const monthly = Object.entries(monthlyMap)
-        //   .map(([month, data]) => ({ month, ...data }))
-        //   .sort((a, b) => {
-        //     const getDate = (str) => new Date(`1 ${str}`);
-        //     return getDate(a.month) - getDate(b.month);
-        //   });
-        // const monthlyMap = {};
-        // transactions.forEach((tx) => {
-        //   const monthKey = tx.date//dayjs(tx.date).utc().format('DD MMM YYYY')//tx.date.toISOString()//dayjs(tx.date).utc().format("MMM YYYY");
-        //   console.log("monthKey", new Date(tx.date).toLocaleDateString("en-GB", {
-        //     // day: "2-digit",
-        //     month: "short",
-        //     year: "numeric",
-        //   }), tx.date, tx.amount, tx.type);
-        //   if (!monthlyMap[monthKey]) {
-        //     monthlyMap[monthKey] = { income: 0, expense: 0 };
-        //   }
-        //   monthlyMap[monthKey][tx.type] += tx.amount;
-        // });
-
         const monthly = Object.entries(monthlyMap)
           .map(([month, data]) => ({ month, ...data }))
           .sort((a, b) => {
