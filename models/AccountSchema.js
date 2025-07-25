460921100a9d16b454fe58b5712cdf0a4c24d530
sayuj <sayuj.sehgal@gmail.com>
2025-07-25 16:59:06 +0530

    refactor: enhance transaction deletion logic in AccountSchema pre-hook

--- models/AccountSchema.js
+++ models/AccountSchema.js
@@ -32,15 +32,24 @@ const AccountSchema = new mongoose.Schema({
 AccountSchema.pre("findOneAndDelete", async function (next) {
   try {
     const account = await this.model.findOne(this.getQuery());
-    if (account) {
-      const Transaction = mongoose.model("Transaction"); // Lazy get model by name
-      await Transaction.deleteMany({ accountId: account._id });
+    if (!account) return next();
+
+    const Transaction = mongoose.model("Transaction");
+
+    // Step 1: Fetch transaction IDs for this account
+    const transactionIds = await Transaction.find({ accountId: account._id }).select("_id");
+
+    // Step 2: Delete each transaction using findOneAndDelete to trigger middleware
+    for (const tx of transactionIds) {
+      await Transaction.findOneAndDelete({ _id: tx._id });
     }
+
     next();
   } catch (error) {
     next(error);
   }
 });
+
 
 const Account =
   mongoose.models.Account || mongoose.model("Account", AccountSchema);
