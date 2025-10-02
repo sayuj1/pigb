67b554c490862363c980fadb8cf8007d0856fc41
sayuj <sayuj.sehgal@gmail.com>
2025-10-02 10:17:50 +0530

    added pigb favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/pigb.ico" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
