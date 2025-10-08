e1bfcda4dc8e812a8cf86cc7995c9750deb88fc3
sayuj <sayuj.sehgal@gmail.com>
2025-10-09 05:17:54 +0530

    add JSON-LD schema for organization and website, and include site logo

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,46 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const SITE_URL = "https://pigb.sehgaltech.com";
+  const SITE_NAME = "PIGB SehgalTech";
+  const LOGO_URL = `${SITE_URL}/logo.png`;
   return (
     <Html lang="en">
       <Head >
+        {/* JSON-LD: Organization schema */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": SITE_URL,
+              "name": SITE_NAME,
+              "logo": LOGO_URL
+            }),
+          }}
+        />
+
+        {/* JSON-LD: WebSite schema */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "url": SITE_URL,
+              "name": SITE_NAME,
+              "publisher": {
+                "@type": "Organization",
+                "name": SITE_NAME,
+                "logo": {
+                  "@type": "ImageObject",
+                  "url": LOGO_URL
+                }
+              }
+            }),
+          }}
+        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
